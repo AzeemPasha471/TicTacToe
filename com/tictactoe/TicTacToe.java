af1c201ca809c8073c1433d4cdee23584170c1b7
Azeempasha471 <abdulazeem1023@gmail.com>
2021-06-30 22:00:10 +0530

    CreateBoard

--- com/tictactoe/TicTacToe.java
+++ com/tictactoe/TicTacToe.java
@@ -1,40 +1,19 @@
 package com.tictactoe;
 
-import java.util.Scanner;
-
 public class TicTacToe {
-	 public static char[] createBoard() {
-		 char [] board = new char[10];
-		 
-		 for(int i=1; i<10; i++) {
-			 board[i]= ' ';
-		 }
+	public static char[] createBoard() {
+		char[] board = new char[10];
+
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
 		return board;
-				 
-		 }
-	      public static void chooseOption () {
-	    	  
-	    	  char computerSymbol = '0';
-	    	   
-	    	   Scanner sc = new Scanner(System.in);
-	    	   System.out.println("give the option 'o' or 'x'");
-	    	   char option = sc.next().charAt(0);
-	    	   
-	    	   if(option =='o') {
-	    		   computerSymbol = 'x';
-	    	   }
-	    	   else if(option == 'x') {
-	    		   computerSymbol='o';
-	    	   }
-	    	   System.out.println(" player: "+option+"\n computer :"+computerSymbol);
-	    				   
-	    	   }
-	    	   
-	      
-	public static void main(String[] args)
-	{
+
+	}
+
+	public static void main(String[] args) {
 		System.out.println("welcome");
-	     char[] board = createBoard();
-	     chooseOption();
+		char[] board = createBoard();
+
 	}
 }
