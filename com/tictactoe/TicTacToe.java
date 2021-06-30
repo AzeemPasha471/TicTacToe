c3e09aeca513ca439b2333a55c7f4760c4db9bcd
Azeempasha471 <abdulazeem1023@gmail.com>
2021-06-30 20:22:48 +0530

    createdboard

---
+++ com/tictactoe/TicTacToe.java
@@ -0,0 +1,19 @@
+package com.tictactoe;
+
+public class TicTacToe {
+	 public static char[] createBoard() {
+		 char [] board = new char[10];
+		 
+		 for(int i=1; i<10; i++) {
+			 board[i]= ' ';
+		 }
+		return board;
+				 
+		 }
+
+	public static void main(String[] args)
+	{
+		System.out.println("welcome");
+	     char[] board = createBoard();
+	}
+}
